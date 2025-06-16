87fc45ef59c77b51a11e8fdfed6723214109286d
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2025-06-17 00:29:57 +0700

    feat: implement sessions management with login, logout, and session handling

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root "home#index"
   get "signup", to: "users#new"
   post "users", to: "users#create"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
